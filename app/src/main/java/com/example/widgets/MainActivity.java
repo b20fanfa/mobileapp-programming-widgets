f5e19edb324fa86fb33ca22d171d0749876c2b95
Fanny Falck <b20fanfa@student.his.se>
2021-04-13 16:18:15 +0200

    skrivit innuti MainActivity, la till så att när användaren skriver något i fältet ska det visas upp.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -25,27 +25,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        name = (EditText) findViewById(R.id.edit_text_name);
-        lname = (EditText) findViewById(R.id.edit_text_last_name);
-        mail = (EditText) findViewById(R.id.edit_text_email);
-        date = (EditText) findViewById(R.id.edit_text_date);
+        //name = (EditText) findViewById(R.id.edit_text_name);
+        //lname = (EditText) findViewById(R.id.edit_text_last_name);
+        //mail = (EditText) findViewById(R.id.edit_text_email);
+        //date = (EditText) findViewById(R.id.edit_text_date);
+
+        //FirstButton = (Button) findViewById(R.id.button1);
 
 
-        FirstButton = (Button) findViewById(R.id.button1);
+      //  FirstButton.setOnClickListener(new View.OnClickListener() {
+
+          //  @Override
+          //  public void onClick(View view) {
+          //      Toast.makeText(getApplicationContext(), "Din information har sparats", Toast.LENGTH_LONG).show();//När användaren har klickat på knappen kommer denna mening upp.
+         //   }
+       // });
 
 
-        FirstButton.setOnClickListener(new View.OnClickListener() {
+//    }
 
+
+//}
+
+//Här hag jag försökt att få upp information från användaren när de skriver in!!!
+        final EditText edit_text_name = (EditText) findViewById(R.id.edit_text_name);//get the id for edit text
+        final EditText edit_text_last_name = (EditText) findViewById(R.id.edit_text_last_name);
+        final EditText edit_text_email = (EditText) findViewById(R.id.edit_text_email);
+        final EditText edit_text_date = (EditText) findViewById(R.id.edit_text_date);
+
+        Button button1 = (Button) findViewById(R.id.button1);//get the id for button
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Din information har sparats", Toast.LENGTH_LONG).show();//När användaren har klickat på knappen kommer denna mening upp.
+                if (edit_text_name.getText().toString() != null)//check whether the entered text is not null
+                {
+                    Toast.makeText(getApplicationContext(), edit_text_name.getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
+                }
+
+                if (edit_text_last_name.getText().toString() != null)//check whether the entered text is not null
+                {
+                    Toast.makeText(getApplicationContext(), edit_text_last_name.getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
+                }
+
+                if (edit_text_email.getText().toString() != null)//check whether the entered text is not null
+                {
+                    Toast.makeText(getApplicationContext(),edit_text_email .getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
+                }
+
             }
         });
-
-
     }
 
 
 }
-
 
