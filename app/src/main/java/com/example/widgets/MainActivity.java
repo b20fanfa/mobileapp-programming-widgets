9ae1d536c1e422ec1df82ac620054c05b539fbef
Fanny Falck <b20fanfa@student.his.se>
2021-04-13 17:07:06 +0200

    skrivit innuti MainActivity: När användaren skriver i de 4 fälten kommer det att visas upp,

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -9,14 +9,14 @@ import android.widget.EditText;
 
 //för knappen//
 import android.widget.Button;
-import android.widget.TextView;
+//import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    EditText name, lname, mail, date ;
+    EditText name, lname, mail, date;
     Button FirstButton;
 
     @Override
@@ -24,58 +24,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        name = (EditText) findViewById(R.id.edit_text_name);
+        lname = (EditText) findViewById(R.id.edit_text_last_name);
+        mail = (EditText) findViewById(R.id.edit_text_email);
+        date = (EditText) findViewById(R.id.edit_text_date);
 
-        //name = (EditText) findViewById(R.id.edit_text_name);
-        //lname = (EditText) findViewById(R.id.edit_text_last_name);
-        //mail = (EditText) findViewById(R.id.edit_text_email);
-        //date = (EditText) findViewById(R.id.edit_text_date);
+        FirstButton = (Button) findViewById(R.id.button1);
 
-        //FirstButton = (Button) findViewById(R.id.button1);
-
-
-      //  FirstButton.setOnClickListener(new View.OnClickListener() {
-
-          //  @Override
-          //  public void onClick(View view) {
-          //      Toast.makeText(getApplicationContext(), "Din information har sparats", Toast.LENGTH_LONG).show();//När användaren har klickat på knappen kommer denna mening upp.
-         //   }
-       // });
-
-
-//    }
-
-
-//}
-
-//Här hag jag försökt att få upp information från användaren när de skriver in!!!
-        final EditText edit_text_name = (EditText) findViewById(R.id.edit_text_name);//get the id for edit text
-        final EditText edit_text_last_name = (EditText) findViewById(R.id.edit_text_last_name);
-        final EditText edit_text_email = (EditText) findViewById(R.id.edit_text_email);
-        final EditText edit_text_date = (EditText) findViewById(R.id.edit_text_date);
-
-        Button button1 = (Button) findViewById(R.id.button1);//get the id for button
-        button1.setOnClickListener(new View.OnClickListener() {
+        FirstButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (edit_text_name.getText().toString() != null)//check whether the entered text is not null
-                {
-                    Toast.makeText(getApplicationContext(), edit_text_name.getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
+            public void onClick(View v) {
+                if (name.getText().toString().isEmpty() || lname.getText().toString().isEmpty() || mail.getText().toString().isEmpty() || date.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Enter the Data", Toast.LENGTH_SHORT).show();
                 }
-
-                if (edit_text_last_name.getText().toString() != null)//check whether the entered text is not null
-                {
-                    Toast.makeText(getApplicationContext(), edit_text_last_name.getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
+                else {
+                    Toast.makeText(getApplicationContext(), "Name -  " + name.getText().toString() + " \n" + "Last name -  " + lname.getText().toString()
+                            + " \n" + "E-Mail -  " + mail.getText().toString() + " \n" + "Date -  " + date.getText().toString()
+                            , Toast.LENGTH_SHORT).show();
                 }
-
-                if (edit_text_email.getText().toString() != null)//check whether the entered text is not null
-                {
-                    Toast.makeText(getApplicationContext(),edit_text_email .getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
-                }
-
             }
         });
     }
-
-
 }
+
+
 
